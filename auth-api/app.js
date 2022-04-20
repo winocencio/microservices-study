cfce2988d9be1ae4e4decfb0f7e44ae1688b665e
Willian Inocencio <willian.i.alves@hotmail.com>
2022-04-20 06:19:33 -0300

    auth-api - connect with db initialize, user folder

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,8 +1,17 @@
 import express from "express";
 
+import * as db from "./src/config/db/initialData.js";
+import userRoutes from "./src/modules/user/routes/UserRoutes.js";
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
+
+db.createInitialData();
+
+app.use(express.json());
+
+app.use(userRoutes);
 
 app.get("/api/status", (req,res) => {
     return res.status(200).json({
---
+++ auth-api/src/modules/user/repository/UserRepository.js
@@ -0,0 +1,24 @@
+import User from "../model/User.js";
+
+class UserRepository {
+
+    async findById(id){
+        try {
+            return await User.findOne({where: {id}});
+        }catch (error){
+            console.error(error.message);
+            return null;
+        }
+    }
+
+    async findByEmail(email){
+        try {
+            return await User.findOne({where: {email}});
+        }catch (error){
+            console.error(error.message);
+            return {test :'aa'};
+        }
+    }
+}
+
+export default new UserRepository();
